4b3bc42afa944125cf24abbd4ce646c8a7c2f201
I Wayan Suardinata <suardinatawayan@gmail.com>
2026-09-08 14:35:21 +0700

    selesai add taskresource

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
@@ -14,7 +15,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return Task::all();
+        // return Task::all();
+        //return TaskResource::collection(Task::all());
+        return Task::all()->toResourceCollection();
     }
 
     /**
